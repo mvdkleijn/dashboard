cbf92de92eb8218b3fca278d33f830e967d00d35
Martijn <martijn.niji@gmail.com>
2011-08-29 15:39:51 +0200

    Added postgresql support

--- enable.php
+++ enable.php
@@ -3,6 +3,7 @@
 /*
  * Dashboard - Wolf CMS dashboard plugin
  *
+ * Copyright (c) 2011 Martijn van der Kleijn <martijn.niji@gmail.com>
  * Copyright (c) 2008-2011 Mika Tuupola
  *
  * Licensed under the MIT license:
@@ -14,10 +15,11 @@
 if (!defined('IN_CMS')) { exit(); }
 
 $pdo = Record::getConnection();
+$driver = $pdo->getAttribute(PDO::ATTR_DRIVER_NAME);
 $table = TABLE_PREFIX."dashboard_log";
 
-if (preg_match('/^mysql/', DB_DSN)) {
-    /* Schema for MySQL */
+/* MySQL schema */
+if ($driver == 'mysql') {
     $pdo->exec("CREATE TABLE $table (
         id          INT(11) NOT NULL AUTO_INCREMENT,
         ident       CHAR(16) NOT NULL,
@@ -28,8 +30,9 @@ if (preg_match('/^mysql/', DB_DSN)) {
         PRIMARY KEY (id)
         ) DEFAULT CHARSET=utf8");
 }
-else {
-    /* Otherwise assume SQLite */
+
+/* SQLite schema */
+if ($driver == 'sqlite') {
     $pdo->exec("CREATE TABLE $table (
         id          INTEGER PRIMARY KEY AUTOINCREMENT,
         ident       CHAR(16) NOT NULL,
@@ -39,3 +42,19 @@ else {
         created_on  DATETIME DEFAULT NULL
         )");
 }
+
+/* PostgreSQL schema */
+if ($driver == 'pgsql') {
+    $pdo->exec("CREATE TABLE $table (
+        id          serial,
+        ident       character varying(16) NOT NULL,
+        priority    integer NOT NULL,
+        message     character varying(255),
+        username    character varying(64),
+        created_on  timestamp DEFAULT NULL,
+        PRIMARY KEY (id)
+        )");
+    
+    $pdo->exec("ALTER SEQUENCE $table id_seq RESTART WITH 1");
+}
+
