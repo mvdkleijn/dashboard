7fb7975e7bf8c7fac209f00debb1fa5be1dc1057
Martijn <martijn.niji@gmail.com>
2011-08-29 15:37:22 +0200

    Added error message if DROP TABLE IF EXISTS failed

--- disable.php
+++ disable.php
@@ -3,6 +3,7 @@
 /*
  * Dashboard - Wolf CMS dashboard plugin
  *
+ * Copyright (c) 2011 Martijn van der Kleijn <martijn.niji@gmail.com>
  * Copyright (c) 2008-2011 Mika Tuupola
  *
  * Licensed under the MIT license:
@@ -14,6 +15,9 @@
 if (!defined('IN_CMS')) { exit(); }
 
 $pdo = Record::getConnection();
-
 $table = TABLE_PREFIX."dashboard_log";
-$pdo->exec("DROP TABLE $table");
+
+if ($pdo->exec("DROP TABLE IF EXISTS $table") === false) {
+    Flash::set('error', __('Unable to drop table :tablename', array(':tablename' => $table)));
+    redirect(get_url('setting'));
+}
