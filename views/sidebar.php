39609664f0e1a550de336f3f123a138cfb53d201
Martijn <martijn.niji@gmail.com>
2011-08-29 12:46:19 +0200

    Slightly reworded good to know section in sidebar

--- views/sidebar.php
+++ views/sidebar.php
@@ -4,7 +4,7 @@
     
 <div class="box">
     <h2><?php echo __('Good to know');?></h2>
-    <p><?php echo __('By default only the latest 10 entries are shown. If you sort or search however, you do so over all entries that exist.');?></p>
+    <p><?php echo __('By default only the 10 latest entries are shown. However, all entries are loaded so if you sort or search, you do so over all entries.');?></p>
     <p><?php echo __('In other words, if you do nothing else, reversing the sort order will display the oldest 10 entries.');?></p>
     <p>
         <?php echo __('Compatible with Wolf CMS :version', array(':version' => '0.7.2+'));?>
